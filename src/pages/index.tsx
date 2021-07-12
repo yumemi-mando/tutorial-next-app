6c36b8cd2a6cbcbfa0136596779cc3766e4ffcb8
Mizue Ando <m_ando@yumemi.co.jp>
2021-07-12 12:33:22 +0900

    add: post on 2021-07-12

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I work for Yumemi as a front-end developer.</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
